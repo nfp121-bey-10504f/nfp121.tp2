13d184b27037b53b8258d7bd2ccc0e9aa51823f5
Maroun Melhem <maroun.melhem@speedlane.co>
2023-12-17 21:14:28 +0200

    feat: question 2 code changes + testing with BlueJ

--- tp2/question2/FahrenheitCelsiusTest.java
+++ tp2/question2/FahrenheitCelsiusTest.java
@@ -1,72 +1,80 @@
 package question2;
 
+import junit.framework.TestCase;
 
 /**
- * Classe-test FahrenheitCelsiusTest.
+ * Classe-test FahrenheitCelsiusTest. Les tests unitaires de FahrenheitCelsius.
  *
- * @author  (votre nom)
- * @version (un numéro de version ou une date)
+ * @author Maroun Melhem
+ * @version 1.0
  *
- * Les classes-test sont documentées ici :
+ * Les classes-test sont documentÃ©es ici :
  * http://junit.sourceforge.net/javadoc/junit/framework/TestCase.html
- * et sont basées sur le document © 2002 Robert A. Ballance intitulé
- * «JUnit: Unit Testing Framework».
+ * et sont basÃ©es sur le document Â© 2002 Robert A. Ballance intitulÃ©
+ * "JUnit: Unit Testing Framework".
  *
- * Les objets Test (et TestSuite) sont associés aux classes à tester
+ * Les objets Test (et TestSuite) sont associÃ©s aux classes Ã  tester
  * par la simple relation yyyTest (e.g. qu'un Test de la classe Name.java
- * se nommera NameTest.java); les deux se retrouvent dans le même paquetage.
- * Les "engagements" (anglais : "fixture") forment un ensemble de conditions
- * qui sont vraies pour chaque méthode Test à exécuter.  Il peut y avoir
- * plus d'une méthode Test dans une classe Test; leur ensemble forme un
- * objet TestSuite.
- * BlueJ découvrira automatiquement (par introspection) les méthodes
- * Test de votre classe Test et générera la TestSuite conséquente.
- * Chaque appel d'une méthode Test sera précédé d'un appel de setUp(),
- * qui réalise les engagements, et suivi d'un appel à tearDown(), qui les
- * détruit.
+ * se nommera NameTest.java); les deux se retrouvent dans le mÃªme paquetage.
+ * Les "fixtures" forment un ensemble de conditions qui sont vraies pour chaque
+ * mÃ©thode Test Ã  exÃ©cuter. Il peut y avoir plus d'une mÃ©thode Test dans une classe
+ * Test; leur ensemble forme un objet TestSuite.
+ * BlueJ dÃ©couvrira automatiquement (par introspection) les mÃ©thodes
+ * Test de votre classe Test et gÃ©nÃ©rera la TestSuite consÃ©quente.
+ * Chaque appel d'une mÃ©thode Test sera prÃ©cÃ©dÃ© d'un appel de setUp(),
+ * qui rÃ©alise les fixtures, et suivi d'un appel Ã  tearDown(), qui les
+ * dÃ©truit.
  */
-public class FahrenheitCelsiusTest extends junit.framework.TestCase
-{
-    // Définissez ici les variables d'instance nécessaires à vos engagements;
-    // Vous pouvez également les saisir automatiquement du présentoir
-    // à l'aide du menu contextuel "Présentoir --> Engagements".
-    // Notez cependant que ce dernier ne peut saisir les objets primitifs
-    // du présentoir (les objets sans constructeur, comme int, float, etc.).
+public class FahrenheitCelsiusTest extends TestCase {
 
     /**
      * Constructeur de la classe-test FahrenheitCelsiusTest
      */
-    public FahrenheitCelsiusTest()
-    {
+    public FahrenheitCelsiusTest() {
     }
 
     /**
-     * Met en place les engagements.
+     * Met en place les fixtures.
      *
-     * Méthode appelée avant chaque appel de méthode de test.
+     * MÃ©thode appelÃ©e avant chaque appel de mÃ©thode de test.
      */
-    protected void setUp() // throws java.lang.Exception
-    {
-        // Initialisez ici vos engagements
-
+    protected void setUp() {
+        // Initialisez ici vos fixtures
     }
 
     /**
-     * Supprime les engagements
+     * Supprime les fixtures
      *
-     * Méthode appelée après chaque appel de méthode de test.
+     * MÃ©thode appelÃ©e aprÃ¨s chaque appel de mÃ©thode de test.
      */
-    protected void tearDown() // throws java.lang.Exception
-    {
-        //Libérez ici les ressources engagées par setUp()
+    protected void tearDown() {
+        // LibÃ©rez ici les ressources engagÃ©es par setUp()
     }
 
     /**
-     * Il ne vous reste plus qu'à définir une ou plusieurs méthodes de test.
-     * Ces méthodes doivent vérifier les résultats attendus à l'aide d'assertions
-     * assertTrue(<boolean>).
-     * Par convention, leurs noms devraient débuter par "test".
-     * Vous pouvez ébaucher le corps grâce au menu contextuel "Enregistrer une méthode de test".
+     * Teste le cas oÃ¹ une NumberFormatException doit Ãªtre levÃ©e.
      */
+    public void testNumberFormatException() {
+        try {
+            FahrenheitCelsius.fahrenheitEnCelsius(Integer.parseInt("NotANumber"));
+            fail("Une NumberFormatException Ã©tait attendue");
+        } catch (NumberFormatException e) {
+            // L'exception attendue a Ã©tÃ© levÃ©e.
+        }
+    }
 
+    /**
+     * Teste la mÃ©thode fahrenheitEnCelsius pour s'assurer que la conversion
+     * est correctement effectuÃ©e avec des entrÃ©es valides.
+     */
+    public void testFahrenheitEnCelsius() {
+        assertEquals("0 degrÃ©s F devrait Ãªtre converti en -17.8 degrÃ©s C",
+                     -17.8,
+                     FahrenheitCelsius.fahrenheitEnCelsius(0),
+                     0.1);
+        assertEquals("100 degrÃ©s F devrait Ãªtre converti en 37.8 degrÃ©s C",
+                     37.8,
+                     FahrenheitCelsius.fahrenheitEnCelsius(100),
+                     0.1);
+    }
 }
